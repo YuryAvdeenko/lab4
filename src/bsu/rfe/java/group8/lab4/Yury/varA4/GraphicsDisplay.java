7b920ae7c39647a446acc35524b0a674a95f7899
YuryAvdeenko <avdeenko.yura@mail.ru>
2020-12-06 20:20:55 +0300

    Adding GraphicsDisplay class

---
+++ src/bsu/rfe/java/group8/lab4/Yury/varA4/GraphicsDisplay.java
@@ -0,0 +1,81 @@
+package bsu.rfe.java.group8.lab4.Yury.varA4;
+
+import java.awt.Stroke;
+import java.awt.font.FontRenderContext;
+
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import javax.swing.JPanel;
+@SuppressWarnings("serial")
+
+public class GraphicsDisplay extends JPanel {
+    // Список координат точек для построения графика
+    private Double[][] graphicsData;
+    // Флаговые переменные, задающие правила отображения графика
+    private boolean showAxis = true;
+    private boolean showMarkers = true;
+    private boolean showIntGraphics = false;
+    // Границы диапазона пространства, подлежащего отображению
+    private double minX;
+    private double maxX;
+    private double minY;
+    private double maxY;
+    // Используемый масштаб отображения
+    private double scale;
+    // Различные стили черчения линий
+    private BasicStroke graphicsStroke;
+    private BasicStroke axisStroke;
+    private BasicStroke markerStroke;
+    private BasicStroke graphicsIntStroke;
+    // Различные шрифты отображения надписей
+    private Font axisFont;
+
+
+public GraphicsDisplay() {
+    // Цвет заднего фона области отображения - белый
+    setBackground(Color.WHITE);
+    // Сконструировать необходимые объекты, используемые в рисовании
+    // Перо для рисования графика
+    graphicsStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
+            BasicStroke.JOIN_ROUND, 10.0f, new float[] {1,1,1,1,1,1,4,1,2,1,2,1}, 0.0f);
+    // Перо для рисования целой части графика
+    graphicsIntStroke = new BasicStroke(3.0f, BasicStroke.CAP_BUTT,
+            BasicStroke.JOIN_ROUND, 10.0f, null, 0.0f);
+    // Перо для рисования осей координат
+    axisStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
+            BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+    // Перо для рисования контуров маркеров
+    markerStroke = new BasicStroke(3.0f, BasicStroke.CAP_BUTT,
+            BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+    // Шрифт для подписей осей координат
+    axisFont = new Font("Serif", Font.BOLD, 36);
+}
+    // Данный метод вызывается из обработчика элемента меню "Открыть файл с графиком"
+    // главного окна приложения в случае успешной загрузки данных
+    public void showGraphics(Double[][] graphicsData) {
+        // Сохранить массив точек во внутреннем поле класса
+        this.graphicsData = graphicsData;
+        // Запросить перерисовку компонента, т.е. неявно вызвать paintComponent()
+        repaint();
+    }
+
+    // Методы-модификаторы для изменения параметров отображения графика
+    // Изменение любого параметра приводит к перерисовке области
+    public void setShowAxis(boolean showAxis) {
+        this.showAxis = showAxis;
+        repaint();
+    }
+
+    public void setShowMarkers(boolean showMarkers) {
+        this.showMarkers = showMarkers;
+        repaint();
+    }
+
+    public void setShowIntGraphics(boolean showIntGraphics) {
+        this.showIntGraphics = showIntGraphics;
+        repaint();
+    }
+
+}
--- src/bsu/rfe/java/group8/lab4/Yury/varA4/Main.java
+++ src/bsu/rfe/java/group8/lab4/Yury/varA4/Main.java
@@ -97,8 +97,7 @@ public class Main extends JFrame {
         graphicsMenu.addMenuListener(new GraphicsMenuListener());
 // Установить GraphicsDisplay в цент граничной компоновки
         getContentPane().add(display, BorderLayout.CENTER);
-    }
-    
+}
 
     public static void main(String[] args) {
 	// write your code here
